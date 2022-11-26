81998865ac9e9eec987bb4bcc7c390da532249c9
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-11-26 08:24:18 +0600

    Connect with Mongodb

--- index.js
+++ index.js
@@ -10,6 +10,22 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sq1hraq.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        const categories = client.db("sneakerv2-0").collection('categories');
+        const products = client.db("sneakerv2-0").collection('products');
+
+        app.get('/categories', async(req, res))
+    }
+    finally {
+        
+    }
+}
+run().catch(console.log);
+
 app.get('/', async (req, res) => {
     res.send('server is running');
 })
