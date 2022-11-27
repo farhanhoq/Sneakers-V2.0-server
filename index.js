16c9d8ff59a4a9925f6137a809d40da5a5fc1937
Farhan Hoque <farhan@Farhans-MacBook-Pro.local>
2022-11-28 03:30:11 +0600

    Load products from Database

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 app.use(cors());
 app.use(express.json());
@@ -17,11 +17,61 @@ async function run() {
     try {
         const categories = client.db("sneakerv2-0").collection('categories');
         const products = client.db("sneakerv2-0").collection('products');
+        const bookings = client.db("sneakerv2-0").collection('bookings');
+        const users = client.db("sneakerv2-0").collection('users');
 
         app.get('/categories', async (req, res) => {
             const query = {};
             const result = await categories.find(query).toArray();
             res.send(result)
+        })
+
+        app.get('/products', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email};
+            const allproducts = await products.find(query).toArray();
+            res.send(allproducts)
+        })
+    
+        app.get('/products/:id', async (req, res) => {
+            const c_ID = req.params.id;
+            const query = { c_ID: c_ID };
+            console.log(query);
+            const result = await products.find(query).toArray();
+            return res.send(result);
+        })
+
+        app.post('/products', async (req, res) => {
+            const product = req.body;
+            const result = await products.insertOne(product);
+            res.send(result)
+        })
+
+        app.post('/bookings', async (req, res) => {
+            const booking = req.body;
+            const result = await bookings.insertOne(booking);
+            res.send(result)
+        })
+
+        app.get('/users', async (req, res) => {
+            const query = {};
+            const allusers = await users.find(query).toArray();
+            res.send(allusers)
+        })
+
+        app.get('/users/role/:email', async (req, res) => {
+            const email = req.params.email;
+            // console.log(email)
+            const query = { email: email };
+            const user = await users.findOne(query);
+            // console.log(user)
+            res.send(user)
+        })
+
+        app.post('/users', async(req, res) => {
+            const user = req.body;
+            const result = await users.insertOne(user);
+            res.send(result);
         })
     }
     finally {
